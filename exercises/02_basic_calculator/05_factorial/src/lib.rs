bb5a553f4a0c7bac0a201339f233620fc3937c39
Arnaud Christ <arnaud.christ@gmail.com>
2024-07-21 10:34:00 +0200

    Basic calculator 05.

--- exercises/02_basic_calculator/05_factorial/src/lib.rs
+++ exercises/02_basic_calculator/05_factorial/src/lib.rs
@@ -10,6 +10,13 @@
 //
 // Use only what you learned! No loops yet, so you'll have to use recursion!
 
+fn factorial(val: u32) -> u32 {
+    if val == 0 {
+        return 1;
+    }
+    val * factorial(val - 1)
+}
+
 #[cfg(test)]
 mod tests {
     use crate::factorial;

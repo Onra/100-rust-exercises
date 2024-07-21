c4ca91c4a21a016bf2be8b46c5a6e42a4bfdeaf6
Arnaud Christ <arnaud.christ@gmail.com>
2024-07-21 10:41:15 +0200

    Basic calculator 06.

--- exercises/02_basic_calculator/06_while/src/lib.rs
+++ exercises/02_basic_calculator/06_while/src/lib.rs
@@ -4,7 +4,19 @@ pub fn factorial(n: u32) -> u32 {
     // interprets as "I'll get back to this later", thus
     // suppressing type errors.
     // It panics at runtime.
-    todo!()
+    let mut i = 0;
+    let mut res = 1;
+
+    while i <= n {
+        if i == 0 {
+            res = res * 1;
+        } else {
+            res = res * i;
+        }
+        i += 1;
+    }
+
+    return res;
 }
 
 #[cfg(test)]
